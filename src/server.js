ccdc3433de4f81d589d1f0a71ebcb47174ec92f0
DenysRogeres <denysmex@gmail.com>
2021-08-12 00:57:48 -0300

    Set view engine

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const server = express();
+const route = require('./route');
+const path = require('path');
+
+
+
+server.set('view engine', 'ejs');
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
 
 server.listen(3000, ()=> {
         console.log("Server is running on port 3000.");
