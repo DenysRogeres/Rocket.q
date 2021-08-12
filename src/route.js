51670484549d7ea769817050388c9da9137c995a
DenysRogeres <denysmex@gmail.com>
2021-08-12 01:40:21 -0300

    Adding pages in routes

--- src/route.js
+++ src/route.js
@@ -4,4 +4,8 @@ const route = express.Router();
 
 route.get('/', (request, response) => response.render("index"));
 
+route.get('/room', (request, response) => response.render("room"));
+
+route.get('/create-pass', (request, response) => response.render("create-pass"));
+
 module.exports = route;
